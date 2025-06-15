b446eb90260bca7a29682fe83ac9cb9e056c3613
Terrance Moncure <terrancemoncure873@gmail.com>
2025-06-15 15:22:58 -0500

    miscilanious stuff

--- run.bat
+++ run.bat
@@ -1,13 +1,11 @@
 cd "C:\Users\terra\Downloads\Projects\repos\Poker"
-cmake -G "MinGW Makefiles" -D CMAKE_CXX_COMPILER="C:/msys64/mingw32/bin/g++.exe" -D CMAKE_BUILD_TYPE=Release -S . -B ./Build
+cmake -G "MinGW Makefiles" -D CMAKE_CXX_COMPILER="C:/msys64/mingw32(13.1.0)/bin/g++.exe" -D CMAKE_BUILD_TYPE=Release -S . -B ./Build
 
 cmake --build build
 cd bin
 
-start server
+start client
 
-start client
-start client
 
 
 
